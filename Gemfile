a8df997eeeccb88b6855841fd954338efab88622
NguyenDuyTa <tandcntt@gmail.com>
2016-12-12 11:03:48 +0900

    Init

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.1.1'
+gem 'sass-rails', '~> 4.0.3'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'devise'
+gem 'private_pub'
+gem 'thin'
+gem 'pg'
+group :production do
+  gem 'rails_12factor'
+end
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+# create default test user
+User.create!(name: "Temona Supporter", email: "supporter@example.com", password: "12345", password_confirmation: "12345")
+User.create!(name: "Nguyen Duy Ta", email: "nguyenduyta@example.com", password: "12345", password_confirmation: "12345")
+User.create!(name: "Customer", email: "customer@example.com", password: "12345", password_confirmation: "12345")
